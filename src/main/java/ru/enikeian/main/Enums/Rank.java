4ac3995b9d21a0542e3078804a86a66835e328c4
Del1gator <yt.reged1t@gmail.com>
2021-09-18 20:19:42 +0300

    Fixing mini error in Rank.java

--- src/main/java/ru/enikeian/main/Enums/Rank.java
+++ src/main/java/ru/enikeian/main/Enums/Rank.java
@@ -1,9 +1,11 @@
 package ru.enikeian.main.Enums;
 
 public enum Rank {
-    private final String nameWithoutColor;
-    private final String nameWithColor;
-    private final String nameWithTab;
-
-    public final String name() {return nameWithoutColor;}
+    PLAYER,
+    PATRON,
+    HELPER,
+    MODERATOR,
+    ELDER_MODERATOR,
+    CO_CREATOR,
+    CREATOR;
 }
